ddf0e5a3fa99ce8d2f897e9e4608b7534334d32e
Mashooq Badar <mashooq.badar@gmail.com>
2021-11-03 18:44:20 +0000

    lesson 3

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -13,12 +13,6 @@
       __
       (recur (dec n) (not acc)))))
 
-(defn recursive-reverse [coll]
-  __)
-
-(defn factorial [n]
-  __)
-
 (meditations
   "Recursion ends with a base case"
   (= true (is-even? 0))
@@ -27,28 +21,4 @@
   (= false (is-even? 1))
 
   "Having too many stack frames requires explicit tail calls with recur"
-  (= false (is-even-bigint? 100003N))
-
-  "Reversing directions is easy when you have not gone far"
-  (= '(1) (recursive-reverse [1]))
-
-  "Yet it becomes more difficult the more steps you take"
-  (= '(6 5 4 3 2) (recursive-reverse [2 3 4 5 6]))
-
-  "Simple things may appear simple."
-  (= 1 (factorial 1))
-
-  "They may require other simple steps."
-  (= 2 (factorial 2))
-
-  "Sometimes a slightly bigger step is necessary"
-  (= 6 (factorial 3))
-
-  "And eventually you must think harder"
-  (= 24 (factorial 4))
-
-  "You can even deal with very large numbers"
-  (< 1000000000000000000000000N (factorial 1000N))
-
-  "But what happens when the machine limits you?"
-  (< 1000000000000000000000000N (factorial 100003N)))
+  (= false (is-even-bigint? 100003N)))
