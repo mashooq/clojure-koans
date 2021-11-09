c79ebcf69d71dd6245bf8cdf22e9c41ba878fdef
Mashooq Badar <mashooq.badar@gmail.com>
2021-11-09 14:47:31 +0000

    lesson 04

--- resources/koans.clj
+++ resources/koans.clj
@@ -21,23 +21,6 @@
                                  street-address ", " city ", " state))
                           ]}]
 
- ["16_refs" {"__" ["hello"
-                "hello"
-                "better"
-                "better!!!"
-                (dosync (ref-set the-world 0))
-                (map :jerry [@the-world @bizarro-world])
-                ]
-          "___" [(fn [x] (+ 20 x))]}]
-
- ["17_atoms" {"__" [0
-                 1
-                 (swap! atomic-clock (partial + 4))
-                 20
-                 20
-                 atomic-clock 20 :fin
-                 ]}]
-
  ["18_quote" {"__" ['(1 2 3 4 5)
                     (1 2 3 4 5)
                     'age
@@ -46,26 +29,7 @@
                     1 2 3
                     1 5]}]
 
- ["19_datatypes" {"__" [(print
-                      (str "You're really the "
-                           (.category this)
-                           ", " recipient "... sorry."))
-                     "peace"
-                     "literature"
-                     "physics"
-                     nil
-                     [true false]
-                     (str "Congratulations on your Best Picture Oscar, "
-                          "Evil Alien Conquerors!")]}]
-
- ["20_java_interop" {"__" [java.lang.String
-                        "SELECT * FROM"
-                        10
-                        1024
-                        ]
-                  "___" [#(.toUpperCase %)
-                         ]}]
-
+ 
  ["21_partition" {"__" [partition
                         [:a :b :c]
                         '((0 1 2) (3 4))
@@ -84,17 +48,6 @@
                                        {:name "Joe" :bad true}]
                         :nice-list [{:name "Jane" :bad false}]}]}]
 
- ["23_meta" {"__" [{:league "National League"}
-                   {:division "West"}
-                   "This doesn't implement the IObj interface"
-                   {:foo :bar}
-                   nil
-                   \C
-                   inc
-                   :park "AT&T Park"
-                   'Giants
-                   "Giants"]}]
-
  ["24_macros" {"__" [~(first form)
                      ~(nth form 2)
                      form
@@ -111,12 +64,6 @@
                                [2 3 4]
                                12
                                [1 2 3]]}]
-
- ["26_transducers" {"__" ['(2 3 4)
-                          [2 4]
-                          [2 4]
-                          [2 4]
-                          6]}]
 
  ["27_multimethods" {"__" ["Hello, World!"
                            "Hello there"
